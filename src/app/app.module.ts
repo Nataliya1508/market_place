2ccc567c37167760628f5c50157d90bf4dbe5e69
Nataly <97055353+Nataliya1508@users.noreply.github.com>
2023-08-21 21:28:15 +0300

    added authorization and login for saler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,17 @@ import { UserController } from '@app/user/user.controller';
 import { UserService } from '@app/user/user.service';
 import { UserEntity } from '@app/user/user.entity';
 import { AuthMiddleware } from '@app/user/middlewares/auth.middleware';
+import { SalerModule } from '@app/saler/saler.module';
+import { SalerEntity } from '@app/saler/saler.entity';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import ormconfig from '@app/ormconfig';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(ormconfig),
-    TypeOrmModule.forFeature([UserEntity]),
+    TypeOrmModule.forFeature([UserEntity, SalerEntity]),
     UserModule,
+    SalerModule,
   ],
   controllers: [UserController, AppController],
   providers: [AppService, UserService],
---
+++ src/saler/saler.service.ts
@@ -0,0 +1,92 @@
+import { CreateUserDto } from '@app/user/dto/createUser.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { SalerEntity } from './saler.entity';
+import { sign } from 'jsonwebtoken';
+import { JWT_SECRET } from '@app/config';
+import { SalerResponseInterface } from '@app/types/salerResponse.interface';
+import { LoginUserDto } from '@app/user/dto/login.dto';
+import { compare } from 'bcrypt';
+
+@Injectable()
+export class SalerService {
+  constructor(
+    @InjectRepository(SalerEntity)
+    private readonly salerRepository: Repository<SalerEntity>,
+  ) {}
+  async createSaler(createSalerDto: CreateUserDto): Promise<SalerEntity> {
+    const salerByEmail = await this.salerRepository.findOne({
+      where: { email: createSalerDto.email },
+    });
+    if (salerByEmail) {
+      throw new HttpException(
+        'Email is already in use ',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    const newSaler = new SalerEntity();
+    Object.assign(newSaler, createSalerDto);
+    console.log('newSaler', newSaler);
+    return await this.salerRepository.save(newSaler);
+  }
+  async login(loginSalerDto: LoginUserDto): Promise<SalerEntity> {
+    const saler = await this.salerRepository.findOne({
+      where: { email: loginSalerDto.email },
+      select: [
+        'id',
+        'companyName',
+        'phoneNumber',
+        'email',
+        'typeSaler',
+        'address',
+        'workingHours',
+        'image',
+        'logo',
+        'aboutUs',
+        'contactPerson',
+        'emailVerified',
+        'isActive',
+      ],
+    });
+    if (!saler) {
+      throw new HttpException(
+        'Invalid email or password',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const isPassswordCorrect = await compare(
+      loginSalerDto.password,
+      saler.password,
+    );
+    if (!isPassswordCorrect) {
+      throw new HttpException(
+        'Invalid email or password',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    delete saler.password;
+    return saler;
+  }
+
+  generateJwt(saler: SalerEntity): string {
+    return sign(
+      {
+        id: saler.id,
+        companyname: saler.companyName,
+        email: saler.email,
+      },
+      JWT_SECRET,
+    );
+  }
+
+  buildSalerResponse(saler: SalerEntity): SalerResponseInterface {
+    return {
+      saler: {
+        ...saler,
+        token: this.generateJwt(saler),
+      },
+    };
+  }
+}
